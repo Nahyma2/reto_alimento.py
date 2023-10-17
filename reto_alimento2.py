15ce19479888a5c57b76729bb2e5718298503795
Nahyma2 <145036199+Nahyma2@users.noreply.github.com>
2023-10-17 09:20:03 -0600

    Create reto_alimento2.py

---
+++ reto_alimento2.py
@@ -0,0 +1,13 @@
+#calcular la siguiente informacion sobre los tacos dorados
+contenido_grasa=float(input("ingrese el contenido de grasa(g por porcion):"))
+contenido_azucar=float(input("ingrese el contenido de azucar(g por porcion):"))
+contenido_sodio=float(input("ingrese el contenido de sodio(mg por porcion):"))
+#evaluar condiciones del taco
+if contenido_grasa<=5
+ contenido_azucar<=10:
+ contenido_sodio<=100:
+print("el alimento tambien es bajo es sodio.")
+if contenido_sodio<=100:
+  print("el alimento es bajo en sodio, pero tiene alto nivel de grasa o azucares.")
+else:
+  print("advertencia: alto nivel de grasa, azucar y sodio. No es saludable.")
